886ace3ed07b371a34106f82d67adf178ec4f693
pchoksy <ds22ec003@eced.svnit.ac.in>
2024-05-27 21:38:15 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,55 +1,3 @@
-# import os
-# import cv2
-# import h5py
-# import numpy as np
-# import torch
-# from torch.utils.data import Dataset
-
-
-# class GazeDataset(Dataset):
-#     def __init__(self, hdf5_path):
-#         self.hdf5_path = hdf5_path
-#         self.data = []
-#         self.labels = []
-
-#         with h5py.File(hdf5_path, 'r') as hdf:
-#             for subject in hdf.keys():
-#                 subject_group = hdf[subject]
-#                 for img_id in subject_group.keys():
-#                     img_group = subject_group[img_id]
-                    
-#                     img_face = img_group['image'][()]  # Original face image
-#                     img_left = img_group['left'][()]    # Normalized left eye image
-#                     img_right = img_group['right'][()]  # Normalized right eye image
-#                     pose = img_group['pose'][()]        # pose
-#                     gaze = img_group['gaze'][()]        # Gaze direction
-
-#                     self.data.append({
-#                         'image': img_face,
-#                         'left_eye': img_left,
-#                         'right_eye': img_right,
-#                         'pose': pose
-#                     })
-                    
-#                     self.labels.append({
-#                         'gaze': gaze
-#                     })
-
-#     def __len__(self):
-#         return len(self.data)
-
-#     def __getitem__(self, idx):
-#         data = self.data[idx]
-#         labels = self.labels[idx]
-
-#         # Convert data and labels to tensors
-#         for key in data:
-#             data[key] = torch.tensor(data[key], dtype=torch.float32)
-#         for key in labels:
-#             labels[key] = torch.tensor(labels[key], dtype=torch.float32)
-
-#         return data, labels
-
 import os
 import h5py
 import numpy as np
@@ -72,7 +20,9 @@ class GazeDataset(Dataset):
                     left_group = subject_group['left']
                     right_group = subject_group['right']
                     pose_group = subject_group['pose']
+                    rot_group = subject_group['rotation']
                     gaze_group = subject_group['gaze']
+                    sub_id_group = subject_group['subject_id']
 
                     for img_id in image_group.keys():  # Iterate through each image ID
                         try:
@@ -80,13 +30,16 @@ class GazeDataset(Dataset):
                             img_left = left_group[img_id][()]    # Normalized left eye image
                             img_right = right_group[img_id][()]  # Normalized right eye image
                             pose = pose_group[img_id][()]        # Pose information
+                            rotation = rot_group[img_id][()]     # head rotation matrix
                             gaze = gaze_group[img_id][()]        # Gaze direction
-
+                            subject_id = sub_id_group[img_id][()] 
                             self.data.append({
                                 'image': img_face,
                                 'left_eye': img_left,
                                 'right_eye': img_right,
-                                'pose': pose
+                                'pose': pose,
+                                'head_rot':rotation
+                                'subject_id': subject_id
                             })
 
                             self.labels.append({
