0f83c4ef1066896aedc3d8e394075cd4ffdae171
pchoksy <ds22ec003@eced.svnit.ac.in>
2024-06-01 18:41:37 +0530

    config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config:
         self.learning_rate = 3e-4
         self.batch_size = 8
         self.num_epochs = 50
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.hdf5_path = '/media/dl/DL/pc/pgte/MPIIFaceGaze_multiregion.h5'
-        self.save_dir = '/media/dl/DL/pc/pgte/weights'
+        self.device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+        self.hdf5_path = './' + '/MPIIFaceGaze_multiregion.h5' #train dataset hdf file
+        self.save_dir = './' + '/weights' # create 'weights' folder
         self.delta = 1.5
 
 config = Config()
